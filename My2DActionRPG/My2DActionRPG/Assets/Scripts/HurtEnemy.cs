159ee3f6b1cb64ce5627b0ea94440897d9de5202
Andrew McClain <35981327+andrewsmcclaingit@users.noreply.github.com>
2018-11-14 22:22:22 -0600

    vid 14
    sword animations

--- My2DActionRPG/My2DActionRPG/Assets/Scripts/HurtEnemy.cs
+++ My2DActionRPG/My2DActionRPG/Assets/Scripts/HurtEnemy.cs
@@ -6,11 +6,25 @@ public class HurtEnemy : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        //tag all enemies as tag so that they have same?
+        if (other.gameObject.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+        }
+
+    }
+       
 }
