54aec5f0db44d3474eb36aa8c6560fe3db8c0c15
Andrew McClain <35981327+andrewsmcclaingit@users.noreply.github.com>
2018-11-29 16:11:11 -0600

    vid 19, hp bar
    vid 19, hp bar

---
+++ My2DActionRPG/My2DActionRPG/Assets/Scripts/UIManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    public Slider healthBar;
+    public Text hpText;
+    public PlayerHealthManager playerHealth;
+    private static bool UIExists;
+
+	// Use this for initialization
+	void Start () {
+
+        //if player does not exist, see line 23
+        if (!UIExists)
+        {
+            UIExists = true;
+            //dont destroy objects on load like transform game object. keeping camera and player the same and move to new area/scene
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            //no duplicate player creations
+            Destroy(gameObject);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        healthBar.maxValue = playerHealth.playerMaxHealth;
+        healthBar.value = playerHealth.playerCurrentHealth; //bar shows current health calling from playerhealth manager
+        //healthBar.minValue = 0f;
+        hpText.text = "HP: " + playerHealth.playerCurrentHealth + "/" + playerHealth.playerMaxHealth;
+	}
+}
